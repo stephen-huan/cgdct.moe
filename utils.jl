01ea7734d920137f944b4974b7f591e67ef28847
Stephen Huan <stephen03.huan@gmail.com>
2022-10-21 17:00:32 -0400

    use Franklin.get_url in hfun_makeheader

--- utils.jl
+++ utils.jl
@@ -4,13 +4,11 @@
 Make the header list for the website.
 """
 function hfun_makeheader()
-    current_page = "/$(splitext(locvar("fd_rpath"))[1])"
-    current_page = (endswith(current_page, "index")) ?
-        current_page[1:end - 5] : "$current_page/"
+    current_url = get_url(locvar("fd_rpath"))
     io = IOBuffer()
     write(io, "<ul>")
     for (url, name) in globvar("headers")
-        is_active = (url == current_page) ? "active" : ""
+        is_active = (url == current_url) ? "active" : ""
         write(io, """<li><a href="$url" class="$is_active">$name</a></li>\n""")
     end
     write(io, "</ul>")
