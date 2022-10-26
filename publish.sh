f609a39bc0a49064b1711f3d04923b2d1a9f1068
Stephen Huan <stephen03.huan@gmail.com>
2022-10-26 02:13:26 -0400

    abort if any command fails in deployment script

--- publish.sh
+++ publish.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # script to deploy the site to GitHub pages
 
-commit="$(git rev-parse --short HEAD)"
 main_branch="master"
 build_branch="build"
 push_branch="gh-pages"
+commit="$(git rev-parse --short HEAD)"
 
-git checkout "$build_branch"
+git checkout "$build_branch" &&
 # sync changes
-git merge "$main_branch"
+git merge --no-verify "$main_branch" &&
 
 # build site
 julia --project="@." -O0 -e "using Franklin: optimize; \
-    optimize(clear=true, prerender=true, minify=false)"
+    optimize(clear=true, prerender=true, minify=false)" &&
 
 # publish
-git add --force __site
-git commit --message "Deploying to $push_branch from @ $commit"
+git add --force __site &&
+git commit --message "Deploying to $push_branch from @ $commit" &&
 # https://gist.github.com/cobyism/4730490
 git subtree push --prefix __site origin "$push_branch"
 
