7955e8fa2c78b322c42be2d717a433af25789d3d
Stephen Huan <stephen03.huan@gmail.com>
2023-03-24 02:03:43 -0400

    feat(utils): add general star macro

--- utils/katex.jl
+++ utils/katex.jl
@@ -12,6 +12,67 @@ function make_def(name)
         \end{wrap}"""
 end
 
+"""
+    starmacro()
+
+Make a macro with a star and a non-star mode.
+
+The extra level of indirection is necessary to parse as a KaTeX macro.
+see:
+- https://latexref.xyz/_005c_0040ifstar.html
+- https://katex.org/docs/supported.html#macros
+"""
+function starmacro(name, num, nostar, star)
+    lxd(name, 0, """
+        \\providecommand{\\$(name)helper}{
+          \\@ifstar{\\$(name)star}{\\$(name)nostar}
+        }
+        \\providecommand{\\$(name)nostar}[$num]{$nostar}
+        \\providecommand{\\$(name)star}[$num]{$star}
+        \\$(name)helper"""
+    )
+end
+
+function starmacro(name, num, nostar)
+    left = ["(", "[", "\\lvert", "\\lVert", "\\langle"]
+    middle = ["|", "\\vert", "\\|", "\\Vert"]
+    right = [")", "]", "\\rvert", "\\rVert", "\\rangle"]
+    star = replace(
+        nostar,
+        [
+            Pair(delim, direction * delim)
+            for (direction, delims) in zip(
+                ("\\left", "\\middle", "\\right"),
+                (left, middle, right)
+            ) for delim in delims
+        ]...,
+    )
+    starmacro(name, num, nostar, star)
+end
+
+"""
+DeclarePairedDelimiterX(name, num, left, right, body)
+
+An emulator of the corresponding mathtools macro.
+"""
+function DeclarePairedDelimiterX(name, num, left, right, body)
+    starmacro(
+        name,
+        num,
+        "$left $body $right",
+        "\\left $left $body \\right $right",
+    )
+end
+
+"""
+DeclarePairedDelimiter(name, left, right)
+
+An emulator of the corresponding mathtools macro.
+"""
+function DeclarePairedDelimiter(name, left, right)
+    DeclarePairedDelimiterX(name, 1, left, right, "#1")
+end
+
 # LaTeX-like math macros.
 # n.b.: these are parsed by Franklin, *not* KaTeX.
 # KaTeX has its own macro system: https://katex.org/docs/supported.html#macros
@@ -44,13 +105,25 @@ const katex_commands = [
     # \Reals and \Complex already provided by KaTeX
     lxd("C", 0, raw"""\mathbb{C}"""),
     # paired delimiters
-    lxd("norm", 1, raw"""\lVert #1 \rVert"""),
-    lxd("fro", 1, raw"""\lVert #1 \rVert_{\operatorname{FRO}}"""),
-    lxd("card", 1, raw"""\lvert #1 \rvert"""),
-    lxd("abs", 1, raw"""\lvert #1 \rvert"""),
-    lxd("inner", 2, raw"""\langle #1, #2 \rangle"""),
-    lxd("floor", 1, raw"""\lfloor #1 \rfloor"""),
-    lxd("ceil", 1, raw"""\lceil #1 \rceil"""),
+    DeclarePairedDelimiter("norm", raw"""\lVert""", raw"""\rVert"""),
+    DeclarePairedDelimiter("card", raw"""\lvert""", raw"""\rvert"""),
+    DeclarePairedDelimiter("abs", raw"""\lvert""", raw"""\rvert"""),
+    DeclarePairedDelimiter("floor", raw"""\lfloor""", raw"""\rfloor"""),
+    DeclarePairedDelimiter("ceil", raw"""\lceil""", raw"""\rceil"""),
+    DeclarePairedDelimiterX(
+        "fro",
+        1,
+        raw"""\lVert""",
+        raw"""\rVert_{\operatorname{FRO}}""",
+        "#1"
+    ),
+    DeclarePairedDelimiterX(
+        "inner",
+        2,
+        raw"""\langle""",
+        raw"""\rangle""",
+        "#1, #2",
+    ),
     # operators
     lxd("argmin", 0, raw"""\operatorname*{argmin}"""),
     lxd("argmax", 0, raw"""\operatorname*{argmax}"""),
@@ -65,31 +138,14 @@ const katex_commands = [
     lxd("mean", 0, raw"""\mean"""),
     lxd("var", 0, raw"""\sigma^2"""),
     lxd("std", 0, raw"""\sigma"""),
-    lxd("Var", 1, raw"""\mathbb{V}\text{ar}[#1]"""),
-    lxd("Cov", 2, raw"""\mathbb{C}\text{ov}[#1, #2]"""),
-    lxd("Corr", 2, raw"""\mathbb{C}\text{orr}[#1, #2]"""),
-    lxd("Entropy", 1, raw"""\mathbb{H}[#1]"""),
-    lxd("MI", 2, raw"""\mathbb{I}[#1; #2]"""),
-    # the extra level of indirection is necessary to parse as a KaTeX macro.
-    # see:
-    # - https://latexref.xyz/_005c_0040ifstar.html
-    # - https://katex.org/docs/supported.html#macros
-    lxd("E", 0, raw"""
-        \providecommand{\Ehelper}{\@ifstar{\Estar}{\Enostar}}
-        \providecommand{\Enostar}[1]{\mathbb{E}       [ #1        ]}
-        \providecommand{\Estar  }[1]{\mathbb{E} \left [ #1 \right ]}
-        \Ehelper"""
-    ),
-    lxd("KL", 0, raw"""
-    \providecommand{\KLhelper}{\@ifstar{\KLstar}{\KLnostar}}
-    \providecommand{\KLnostar}[2]{
-        \mathbb{D}_{\operatorname{KL}}       ( #1 \;         \| \; #2        )
-    }
-    \providecommand{\KLstar  }[2]{
-        \mathbb{D}_{\operatorname{KL}} \left ( #1 \; \middle \| \; #2 \right )
-    }
-    \KLhelper"""
-    ),
+    starmacro("E", 1, raw"""\mathbb{E}[#1]"""),
+    starmacro("Ep", 2, raw"""\mathbb{E}_{#1}[#2]"""),
+    starmacro("Var", 1, raw"""\mathbb{V}\text{ar}[#1]"""),
+    starmacro("Cov", 2, raw"""\mathbb{C}\text{ov}[#1, #2]"""),
+    starmacro("Corr", 2, raw"""\mathbb{C}\text{orr}[#1, #2]"""),
+    starmacro("Entropy", 1, raw"""\mathbb{H}[#1]"""),
+    starmacro("MI", 2, raw"""\mathbb{I}[#1; #2]"""),
+    starmacro("KL", 2, raw"""\mathbb{D}_{\operatorname{KL}}(#1\; \| \;#2)"""),
 ]
 
 const katex_environments = append!(
